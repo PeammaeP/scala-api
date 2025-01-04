87bfc9471af42051b21ca17d49aed1c9cae0e88c
PeamMiiZ <peemmygg@gmail.com>
2025-01-04 21:04:26 +0700

    add mongodb installation

---
+++ src/main/scala/EntryPointMongo.scala
@@ -0,0 +1,13 @@
+import org.mongodb.scala._
+import org.mongodb.scala.model.Aggregates._
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Sorts._
+import org.mongodb.scala.model.Updates._
+import org.mongodb.scala.model._
+
+import scala.collection.JavaConverters._
+
+object EntryPointMongo {
+  
+}
